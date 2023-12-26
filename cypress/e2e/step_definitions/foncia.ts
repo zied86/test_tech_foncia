182d40da85a87ff5f073f749c61692b0d0e7a4ec
Zied RAISSI <raissi.zied0108@gmail.com>
2023-12-26 18:56:09 +0100

    add all test

--- cypress/e2e/step_definitions/foncia.ts
+++ cypress/e2e/step_definitions/foncia.ts
@@ -19,3 +19,13 @@ Then("La page d'accueil doit comporter toutes les rubriques", function () {
     homePage.clickMenuButton();
     homePage.verifySections();
   });
+
+
+Given("L'utilisateur effectue une recherche avec les critères Louer un appartement à {string} avec un loyer MAX {string}€", function (city:string, price:string) {
+  homePage.selectproject(city,price)
+  });
+
+Then("Les résultats de la recherche devraient inclure des appartements avec un loyer maximal de {string}€",function(maxPrice:number){
+  homePage.checkValuesOnAllPages(maxPrice)
+
+});
--- cypress/pages/foncia.ts
+++ cypress/pages/foncia.ts
@@ -15,11 +15,26 @@ const expectedSectionsSelector = {
   "Découvrir Foncia": ":nth-child(7) > .header-tab-link",
   "Carrières": ":nth-child(8) > .header-tab-link",
 };
+const projectSelector = "#projectToggle";
+const selectLouer = ":nth-child(2) > .p-radiobutton-label";
+const selectPropertyType = '#propertyToggle';
+const selectApartment = '.popup-form > :nth-child(1) > :nth-child(1) > .p-checkbox-label';
+const maxPriceSelector = "#price";
+const searchButtonSelector = ".button > :nth-child(2) > .p-button";
+const citySelector = ".p-autocomplete > .ng-tns-c57-16";
+const selectPris = "#pr_id_6_list > :nth-child(2) > .ng-tns-c57-16";
+const acceptCookies = ".cookie-cta-accept > .p-button";
+const listeOfAllValues = "div.foncia-card-price";
+const totalSearchResults = ".search-results-header-total > .search-results-header-value";
+const pagination = ".map-pagination li"
+const nextPageSelector = ".page-link-rel";
+
 
 ////////////////////////////////////////
 // Méthodes
 ///////////////////////////////////////
 class HomePage {
+
   // Accepter les cookies
   acceptCookies() {
     cy.get(acceptCookiesSelector).click();
@@ -51,6 +66,83 @@ class HomePage {
       }
     });
   }
+
+  selectproject(city: string, price: string) {
+    cy.get(projectSelector).click();
+    cy.get(selectLouer).click();
+    cy.get(selectPropertyType).click();
+    cy.get(selectApartment).click();
+    cy.get(maxPriceSelector).type(price);
+    cy.get(citySelector).type(city);
+    cy.get(selectPris).click();
+
+  }
+
+  checkValuesOnAllPages(maxPrice:number) {
+    let listOfValues: any = [];
+    cy.get(acceptCookies).click();
+    cy.get(searchButtonSelector).click();
+    cy.get(totalSearchResults).invoke('text').then((text: string) => {
+      // Faites quelque chose avec le texte récupéré
+      cy.log(`Le texte du champ totalSearchResults est : ${text}`);
+      let totalResultat: number = parseInt(text, 10);
+      cy.log("totalResultat", totalResultat);
+      if (totalResultat != 0 && totalResultat <= 15) {
+
+        cy.get(listeOfAllValues).invoke('text').then((valuesText) => {
+          const valuesArray = valuesText.split('€/ mois CC');
+
+          valuesArray.forEach((value) => {
+            if (value != '') {
+              listOfValues.push(value.replace(',', '.').replace(/\s+/g, '').replace(' ', ''));
+            }
+          });
+          for (let i = 0; i < listOfValues.length; i++) {
+            //  listOfValues[i].replace('€/ mois CC', ' ');
+            let rentPrice = parseFloat(listOfValues[i]);
+            expect(parseFloat(maxPrice)).to.be.greaterThan(rentPrice);
+            cy.log(`Assertion réussie pour la valeur : ${maxPrice}`);
+            cy.log(`Assertion réussie pour la valeur : ${listOfValues[i]}`);
+          }
+
+        });
+
+      } else {
+        cy.viewport(1920, 1080); // Résolution Full HD
+        let getPagination = cy.get(pagination)
+        getPagination.its('length').then((paginationLength) => {
+
+          for (let i: any = 1; i <= paginationLength; i++) {
+            // Votre logique pour chaque élément de la pagination
+
+            let listOfValues: any = [];
+            const getallvalues = cy.get(listeOfAllValues).invoke('text').then((valuesText) => {
+              // Supprimez '€/ mois CC' de chaque valeur et ajoutez-la à listOfValues
+              const valuesArray = valuesText.split('€/ mois CC');
+
+              valuesArray.forEach((value) => {
+                if (value != '') {
+                  listOfValues.push(value);
+                }
+              });
+              for (let i = 0; i < listOfValues.length; i++) {
+
+                let rentPrice = parseFloat(listOfValues[i].replace(',', '.').replace(/\s+/g, '').replace(' ', ''));
+                expect(parseFloat(maxPrice)).to.be.greaterThan(rentPrice);
+                cy.log(`Assertion réussie pour la valeur : ${(listOfValues[i].replace(',', '.').replace(/\s+/g, ''))}`);
+              }
+              cy.get(nextPageSelector).click();
+              cy.get(nextPageSelector).click();
+
+            });
+          }
+        });
+      }
+    });
+
+  }
 }
+
+
 
 export const homePage = new HomePage();
